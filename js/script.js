527d8c806220c67dac0c665dcc3b82bfc16a77ff
profAlbertoAyres <alberto.benicio@ifro.edu.br>
2023-04-28 15:07:10 -0400

    Aula 28/04

--- js/script.js
+++ js/script.js
@@ -1,4 +1,10 @@
 function abrirMenu(){
     const divMenu = document.getElementById('menuResp').classList;
     divMenu.toggle('abrir');
+    const ic = document.getElementById('icone');
+    if(divMenu.contains('abrir')){
+        ic.innerHTML='close';
+    }else{
+        ic.innerHTML='menu';
+    }
 }
